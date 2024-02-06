f2b1509994e6a6582c70d273ddb8c3c336e4c255
stewartHutchins <stewartmhutchins@gmail.com>
2024-02-06 23:26:16 +0000

    Refactor: Rename argparser fixture.

--- {{cookiecutter.project_name}}/tests/unit/args_test.py
+++ {{cookiecutter.project_name}}/tests/unit/args_test.py
@@ -5,6 +5,6 @@ import pytest
 from {{cookiecutter.package_name}}.args import create_arg_parser
 
 
-@pytest.fixture
-def argparser() -> ArgumentParser:
+@pytest.fixture(name="arg_parser")
+def arg_parser_fixture() -> ArgumentParser:
     return create_arg_parser()
